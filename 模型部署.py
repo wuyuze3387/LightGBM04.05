705fc654b91789a04955423236c6e2dfdd66850e
wuyuze3387 <1416877065@qq.com>
2025-04-05 21:09:31 +0800

    Update 模型部署.py

--- 模型部署.py
+++ 模型部署.py
@@ -59,7 +59,6 @@ st.sidebar.header("变量输入区域")
 st.sidebar.write("请输入变量值：")
 
 feature_values = []
-feature_names = []
 for feature, properties in feature_ranges.items():
     if properties["type"] == "numerical":
         value = st.sidebar.number_input(
@@ -74,7 +73,6 @@ for feature, properties in feature_ranges.items():
             options=properties["options"],
         )
     feature_values.append(value)
-    feature_names.append(feature)
 
 # 转换为模型输入格式
 features = np.array([feature_values])
@@ -93,20 +91,53 @@ if st.button("预测"):
     base_value = explainer.expected_value
     shap_values_sample = shap_values[0]
 
-    # 更新matplotlib字体设置，确保SHAP力图使用正确的字体
-    plt.rcParams.update({'font.size': 10, 'font.family': 'Microsoft YaHei'})
+    # 定义特征名称和其对应的值
+    features_with_values = np.array([
+    f"Age={feature_values[0]}",
+    f"Weight={feature_values[1]}",
+    f"Residence={feature_values[2]}",
+    f"Marriage={feature_values[3]}",
+    f"Employment={feature_values[4]}",
+    f"Education={feature_values[5]}",
+    f"Insurance={feature_values[6]}",
+    f"Pregnancies={feature_values[7]}",
+    f"Deliveries={feature_values[8]}",
+    f"Delivery Method={feature_values[9]}",
+    f"Adverse Pregnancy History={feature_values[10]}",
+    f"Terminated Pregnancy={feature_values[11]}",
+    f"Pregnancy Weeks={feature_values[12]}",
+    f"Comorbidities={feature_values[13]}",
+    f"Complications={feature_values[14]}",
+    f"Feeding={feature_values[15]}",
+    f"Newborn Defects={feature_values[16]}",
+    f"Monthly Income Per Capita={feature_values[17]}",
+    f"Painless Childbirth={feature_values[18]}",
+    f"Intra_pain={feature_values[19]}",
+    f"Post_Pain={feature_values[20]}",
+    f"Care Methods={feature_values[21]}",
+    f"Sleep Quality={feature_values[22]}",
+    f"Sleep Time={feature_values[23]}",
+    f"Fatigue={feature_values[24]}",
+    f"Activity={feature_values[25]}",
+    f"Dep={feature_values[26]}",
+    f"Anx={feature_values[27]}",
+    f"Intrusive Rumination={feature_values[28]}",
+    f"Purposeful Rumination={feature_values[29]}",
+    f"Resilience={feature_values[30]}",
+    f"Fami_Supp={feature_values[31]}"
+])
 
-    # 创建SHAP力图，确保中文显示
-    plt.figure(figsize=(20, 6))  # 设置图形尺寸为20x6英寸
-    shap.force_plot(
-        base_value, 
-        shap_values_sample, 
-        features[0], 
-        feature_names=feature_names,  # 传递特征名称
-        matplotlib=True,  # 使用Matplotlib显示
-        show=False  # 不显示默认的力图窗口
-    )
+# 创建SHAP力图，确保中文显示
+plt.figure(figsize=(20, 6))  # 设置图形尺寸为20x6英寸
+shap.force_plot(
+    base_value, 
+    shap_values_sample, 
+    features_with_values, 
+    matplotlib=True,  # 使用Matplotlib显示
+    show=False  # 不显示默认的力图窗口
+)
 
-    # 保存SHAP力图并展示
-    plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=600)
-    st.image("shap_force_plot.png")
+
+# 保存SHAP力图并展示
+plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=600)
+st.image("shap_force_plot.png")
